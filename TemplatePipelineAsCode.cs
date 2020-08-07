ad256838fc3f02d9eab8cc888c5f3529a683b500
jacace <javierandrescaceres@gmail.com>
2020-08-07 13:30:25 +0100

    Fixed formatting

--- TemplatePipelineAsCode.cs
+++ TemplatePipelineAsCode.cs
@@ -10,23 +10,35 @@ namespace sample_design_patterns
 
         //In this Step usually the clean-up, cred setup and git clone takes place
         public abstract void Prepare(String repoApiToken, String repoURL, String branch);
+
         //In this function the correct compiler is used, e.g.: tsc or CoffeeScript 
         public abstract void BuildNode();
+
         //In this fucntion typically a static analysis or linter takes place
         public abstract void LintSource();
+
         //In this step static policies can be verified, e.g.: with OPA
         public abstract void RunPolicyAsCode();
+
         public abstract void PackageAssets();
+
         public abstract void Deploy();
+
         public abstract void UnitTest();
+
         //Depending on the domain this migth include BDD with behave or ServerSpec, InSpec, etc
         public abstract void IntegrationTest();
+
         
         //In this step we typically metadata in a supply chain like Grafeas
         public abstract void RecordMetadata();
+
         public abstract void CreateRelease();
+
         public abstract void CryptoSignAssets();
+
         public abstract void Destroy();
+        
         public void Build(String repoApiToken, String repoURL, String branch)
         {
             Prepare(repoApiToken, repoURL, branch);
