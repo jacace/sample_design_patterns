7fff302c7c09a9181fae9ea8ad7e0d241dec569d
jacace <jacace@jacaces-MacBook-Air.local>
2021-01-19 18:55:58 +0000

    Added sample entry point

--- Program.cs
+++ Program.cs
@@ -8,7 +8,14 @@ namespace sample_design_patterns
     {
         static void Main(string[] args)
         {
+            //1-Sample of Template Method Design Pattern
+            CommodityNodejsPipelineAsCode samplePipeline=new CommodityNodejsPipelineAsCode();
+            samplePipeline.Run(null, null, null);
             Console.WriteLine("Sample call!");
+
+            //2-Sample of Strategy Design Pattern
+            CICDPipeline pipeline=new CICDPipeline(new CICDBuildStragey());
+            pipeline.Build();
         }
     }
 }
